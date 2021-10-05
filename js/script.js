ad695dbf06a34e7f4d10e700284dc1c1612bf293
Dinesh <pr760677@gmail.com>
2021-10-05 13:18:40 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+var inner_card = document.getElementById('inner_card');
+
+
+// card2.style.display='none';
+
+card_flip()
+
+
+function card_flip() {
+  
+    inner_card.style.animation='flipp 4s'
+    inner_card.style.animationFillMode='forwards'
+    inner_card.style.animationDelay='1s'
+
+}
+
